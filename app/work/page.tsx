24cbad28a75ca1ef66955fc7827d28da03cef0d8
sethdaking <lastemail768@gmail.com>
2024-10-03 20:12:25 +0200

    Ayyy

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -163,7 +163,8 @@ const Work = () => {
               xl:bottom-0 z-20 w-full justify-between xl:w-max
               xl:justify-none'
               btnStyles='bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px]
-              h-[44px] flex justify-center items-center transition-all' />
+              h-[44px] flex justify-center items-center transition-all' 
+              iconsStyles='bg-accent'/>
             </Swiper>
           </div>
         </div>
