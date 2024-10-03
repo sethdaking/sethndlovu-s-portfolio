5de6b8e2cb7049309f86b78c1248062c87ea56c1
sethdaking <lastemail768@gmail.com>
2024-10-03 20:15:45 +0200

    Ayyy

--- backend/sendEmail.ts
+++ backend/sendEmail.ts
@@ -1,22 +1,31 @@
 import emailjs from 'emailjs-com';
 
-export const sendEmail = (formData) => {
-    const templateParams = {
-      name: formData.name,
-      email: formData.email,
-      user_message: formData.message,
-      service: formData.service,
-      subject: `New message from ${formData.name} regarding ${formData.service}`,
-    };
-  
-    return emailjs.send('service_fbzdxec', 'template_ig9z1p8', templateParams, 'fTnQk8TZAaFn508ci')
-      .then((response) => {
-        console.log('Email sent successfully!');
-        console.log('SUCCESS!', response.status, response.text);
-        return true;
-      }, (error) => {
-        console.log('Error sending email:', error);
-        console.log('FAILED...', error);
-        return false;
-      });
-  }
+// Define a type for form data
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+  service: string;
+}
+
+export const sendEmail = (formData: FormData) => {
+  const templateParams = {
+    name: formData.name,
+    email: formData.email,
+    user_message: formData.message,
+    service: formData.service,
+    subject: `New message from ${formData.name} regarding ${formData.service}`,
+  };
+
+  return emailjs.send('service_fbzdxec', 'template_ig9z1p8', templateParams, 'fTnQk8TZAaFn508ci')
+    .then((response) => {
+      console.log('Email sent successfully!');
+      console.log('SUCCESS!', response.status, response.text);
+      return true;
+    })
+    .catch((error) => { // Changed to catch for better readability
+      console.log('Error sending email:', error);
+      console.log('FAILED...', error);
+      return false;
+    });
+};
