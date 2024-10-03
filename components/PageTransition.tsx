9617d459ee7944724ab251f253cf4c3f9025826f
sethdaking <lastemail768@gmail.com>
2024-10-03 20:19:00 +0200

    Ayyy

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,28 +1,31 @@
 'use client';
 
-import React from 'react'
+import React, { ReactNode } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
 
-const PageTransition = ({children}) => {
-    const  pathname = usePathname();
-
-  return (
-    <AnimatePresence>
-        <div key={pathname}>
-            <motion.div 
-            initial={{opacity: 1}} 
-            animate={{
-                opacity: 0, 
-                transition: {delay: 1, duration: 0.4, ease: "easeInOut"}
-                }}
-                className='h-screen w-screen fixed bg-primary top-0 cursor-pointer-events-none pointer-events-none'
-                />
-            {children}
-        </div>
-        
-    </AnimatePresence>
-  )
+interface PageTransitionProps {
+    children: ReactNode; // Specify the type of children
 }
 
-export default  PageTransition;
+const PageTransition: React.FC<PageTransitionProps> = ({ children }) => {
+    const pathname = usePathname();
+
+    return (
+        <AnimatePresence>
+            <div key={pathname}>
+                <motion.div 
+                    initial={{ opacity: 1 }} 
+                    animate={{
+                        opacity: 0, 
+                        transition: { delay: 1, duration: 0.4, ease: "easeInOut" }
+                    }}
+                    className='h-screen w-screen fixed bg-primary top-0 cursor-pointer-events-none pointer-events-none'
+                />
+                {children}
+            </div>
+        </AnimatePresence>
+    );
+}
+
+export default PageTransition;
